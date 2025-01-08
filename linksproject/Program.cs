3412d5a8d196a537ba2b0f9ad29b043ba6bb97ca
riki250 <riki107297@gmail.com>
2025-01-08 20:24:02 +0200

    הודעה שמתארת את השינויים שביצעת

--- linksproject/Program.cs
+++ linksproject/Program.cs
@@ -12,13 +12,12 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddControllers();
-        //קטגוריה
+        builder.Services.AddSwaggerGen();
 
+        //קטגוריה
         builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
         builder.Services.AddScoped<ICategoryService, CategoryService>();
         //תגובות
@@ -33,14 +32,21 @@ internal class Program
         //דאטה קונטכס
         builder.Services.AddDbContext<DataContext>();
 
-        //var app = builder.Build();
+        var app = builder.Build();
 
-        //app.UseHttpsRedirection();
+        app.UseHttpsRedirection();
 
-        //app.UseAuthorization();
+        app.UseAuthorization();
 
-        //app.MapControllers();
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            c.RoutePrefix = string.Empty;
+        });
 
-        //app.Run();
+        app.MapControllers();
+
+        app.Run();
     }
 }
